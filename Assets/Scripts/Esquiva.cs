a5debfd1604dad71cec1ccd0daa295761a4f754d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-24 09:32:38 -0300

    First Commit

---
+++ Assets/Scripts/Esquiva.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esquiva : MonoBehaviour {
+
+    public static bool esquivar = false;
+
+    public void AtivarEsquiva() {
+        esquivar = true;
+    }
+}
