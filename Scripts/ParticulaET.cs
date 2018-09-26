67d4c3827e47ac222932ce104b8345f4d33e0475
Artutstuts <43538562+Artutstuts@users.noreply.github.com>
2018-09-25 22:50:56 -0300

    updated scripts

---
+++ Scripts/ParticulaET.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticulaET : MonoBehaviour {
+
+	public void Particulas(int i) {
+        if (i == 1) {
+            GameObject.Find("Gas_Venenoso").GetComponent<ParticleSystem>().Play();
+        } else {
+            GameObject.Find("Gas_Venenoso").GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
---
+++ Scripts/UISeguirPers.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISeguirPers : MonoBehaviour {
+
+    public GameObject obj;
+    public Camera camera;
+    RectTransform rt;
+    public float posy;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+        rt = gameObject.GetComponent<RectTransform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 pos = RectTransformUtility.WorldToScreenPoint(camera, new Vector3(obj.transform.position.x, obj.transform.position.y+posy, obj.transform.position.z));
+        rt.position = pos;
+
+		
+	}
+}
